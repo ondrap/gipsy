0ec0fbadf742028f4f6cd74d9d298e573f6c148c
Ondrej Palkovsky <ondrap@penguin.cz>
2010-05-09 11:54:22 +0200

    Fix do firefox 6.

--- xpc/gpslib/gpstest.cpp
+++ xpc/gpslib/gpstest.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
 #include "gps.h"
+#include "garmin.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    Gps *gps = make_gps("/dev/ttyUSB0", GPS_MLR);
-    gps->download_tracklog(NULL, NULL);
-    
-    return 0;
-    
-	cout << "Getting ports" << endl;
+    cout << "Getting ports" << endl;
     PortList ports = get_ports(true);
 
     for (unsigned int i=0; i < ports.size(); i++) {
@@ -20,11 +16,12 @@ int main(int argc, char **argv)
 	try {
 	    Gps *gps = make_gps(ports[i].device, GPS_GARMIN);
 	    if (gps) {
+		GarminGps *ggps = (GarminGps *) gps;
 		cout << "GPS type: " << gps->gpsname << ", unitid: " << gps->gpsunitid << endl;
+		cout << "Garmin ID: " << ggps->gpsid << ", version: " << ggps->gpsversion << endl;
 	    } else {
 		cout << "GPS open failed" << endl;
 	    }
-	    gps->download_tracklog(NULL, NULL);
 	} catch (Exception e) {
 	    cout << "Got exception, GPS open failed: " << e.error << endl;
 	}
