1af336ac8f66410865af1eca7f9078d1e98040ef
Ondrej Palkovsky <ondrap@penguin.cz>
2011-04-14 13:34:29 +0200

    Variable scoping in overlay.

--- extension/chrome/content/overlays-ff.js
+++ extension/chrome/content/overlays-ff.js
@@ -1,21 +1,23 @@
-Components.utils.import("resource://gipsy/util.jsm");
+{
+    Components.utils.import("resource://gipsy/util.jsm");
 
-var myId    = "gipsy-button"; // ID of button to add
-var afterId = "search-container";    // ID of element to insert after
-var navBar  = document.getElementById("nav-bar");
+    let myId    = "gipsy-button"; // ID of button to add
+    let afterId = "search-container";    // ID of element to insert after
+    let navBar  = document.getElementById("nav-bar");
 
-if (navBar && !get_bool_pref('btn_toolbar_added')) {
-    var curSet  = navBar.currentSet.split(",");
-    if (curSet.indexOf(myId) == -1) {
-        var pos = curSet.indexOf(afterId) + 1 || curSet.length;
-        var set = curSet.slice(0, pos).concat(myId).concat(curSet.slice(pos));
+    if (navBar && !get_bool_pref('btn_toolbar_added')) {
+        let curSet  = navBar.currentSet.split(",");
+        if (curSet.indexOf(myId) == -1) {
+            let pos = curSet.indexOf(afterId) + 1 || curSet.length;
+            let set = curSet.slice(0, pos).concat(myId).concat(curSet.slice(pos));
 
-        navBar.setAttribute("currentset", set.join(","));
-        navBar.currentSet = set.join(",");
-        document.persist(navBar.id, "currentset");
-        try {
-            BrowserToolboxCustomizeDone(true);
-        } catch (e) {}
-        set_bool_pref('btn_toolbar_added', true);
+            navBar.setAttribute("currentset", set.join(","));
+            navBar.currentSet = set.join(",");
+            document.persist(navBar.id, "currentset");
+            try {
+                BrowserToolboxCustomizeDone(true);
+            } catch (e) {}
+            set_bool_pref('btn_toolbar_added', true);
+        }
     }
 }
