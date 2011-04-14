381b96edf51e1a20e775b653a8beeeac0ade13b6
Ondrej Palkovsky <ondrap@penguin.cz>
2011-04-14 13:39:55 +0200

    Added scoping for utils.import in overlay.

--- extension/chrome/content/overlays-ff.js
+++ extension/chrome/content/overlays-ff.js
@@ -1,11 +1,12 @@
 {
-    Components.utils.import("resource://gipsy/util.jsm");
+    let util = {};
+    Components.utils.import("resource://gipsy/util.jsm", util);
 
     let myId    = "gipsy-button"; // ID of button to add
     let afterId = "search-container";    // ID of element to insert after
     let navBar  = document.getElementById("nav-bar");
 
-    if (navBar && !get_bool_pref('btn_toolbar_added')) {
+    if (navBar && !util.get_bool_pref('btn_toolbar_added')) {
         let curSet  = navBar.currentSet.split(",");
         if (curSet.indexOf(myId) == -1) {
             let pos = curSet.indexOf(afterId) + 1 || curSet.length;
@@ -17,7 +18,7 @@
             try {
                 BrowserToolboxCustomizeDone(true);
             } catch (e) {}
-            set_bool_pref('btn_toolbar_added', true);
+            util.set_bool_pref('btn_toolbar_added', true);
         }
     }
 }
